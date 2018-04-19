8f9452889a7be439c524228a76470c88eb6d0a5f
Shreyas Bangera <shreyas.bangera@robosoftin.com>
2018-04-19 18:37:09 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class Onboarding extends StatelessWidget {
       child: new ListView(
         children: <Widget>[
           new Container(
-            child: new Image(image: new AssetImage("assets/lake.jpg")),
+            child: new Image(image: new AssetImage("assets/online-shop-4.png")),
             padding: new EdgeInsets.only(top: 80.0, left: 20.0, right: 20.0, bottom: 40.0),
             width: 200.0,
             height: 200.0,
@@ -56,18 +56,36 @@ class Onboarding extends StatelessWidget {
             ),
           ),
           new Padding(
-            padding: const EdgeInsets.only(top: 15.0),
+            padding: const EdgeInsets.only(top: 30.0),
             child: new Center(
-              widthFactor: 0.5,
-              child: new RaisedButton(
-                color: Colors.green,
-                textColor: Colors.white,
-                padding: new EdgeInsets.symmetric(vertical: 10.0),
-                onPressed: () {
-                  FocusScope.of(context).requestFocus(new FocusNode());
-                },
-                child: new Text('Sign in'),
+              child: new Container(
+                width: 200.0,
+                child: new RaisedButton(
+                  shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.all(new Radius.circular(10.0))),
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  padding: new EdgeInsets.symmetric(vertical: 15.0),
+                  onPressed: () {
+                    FocusScope.of(context).requestFocus(new FocusNode());
+                  },
+                  child: new Text('Sign in', style: new TextStyle(fontSize: 20.0),),
+                ),
               ),
+            ),
+          ),
+          new Center(
+            child: new Column(
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: new FlatButton(onPressed: null, child: new Text("Forgot password?", style: new TextStyle(color: Colors.green, fontSize: 16.0))),
+                ),
+                new Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: new FlatButton(onPressed: null, child: new Text("Don't have an account?", style: new TextStyle(fontSize: 16.0))),
+                ),
+                new FlatButton(onPressed: null, child: new Text("Register", style: new TextStyle(color: Colors.green, fontSize: 16.0),))
+              ],
             ),
           )
         ],
